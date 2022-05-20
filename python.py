09068ac9a31690e65558c66c0b38ee58301f6014
beicon <carlosalbertomillanc@gmail.com>
2022-05-19 23:53:03 -0500

    crear carpetas con .mkdir en serie con meses y dias del año

--- python.py
+++ python.py
@@ -1,1 +1,18 @@
-# sumar a y b 
+import calendar
+from pathlib import Path
+
+month = list(calendar.month_name [1:])
+day = list['monday','thursday','wednesday','tuesday','friday','saturday','sunday']
+for i,month in enumerate(month):
+    for day_name in day:
+    
+     Path(f'2022/{i+1}.{month}/{day_name}').mkdir(parents=True, exist_ok=True)
+
+
+
+
+
+
+
+
+       
