8b90dbc85cf6e476e419d7687dfb95438c1cd139
beicon <84368392+beicongame@users.noreply.github.com>
2022-05-22 22:22:49 -0500

    instalacion de antes calculo de area y numero de antenas

--- reto1.py
+++ reto1.py
@@ -1,23 +1,22 @@
-antena_a = 54000
-antena_b = 15
-antena_c = 20
-antena_d = 25
-antena_e = 30
+antena_a = 44600
+antena_b = 51800
+antena_c = 9600
+antena_d = 15300
+antena_e = 13900
 antena_instalada= 36900
 
-
-def antena_instalada2 (antena_a, antena_b, antena_c, antena_d, antena_e):
-    if antena_a == 0:
-        antena_instalada = antena_b + antena_c + antena_d + antena_e
-    elif antena_b == 0:
-        antena_instalada = antena_a + antena_c + antena_d + antena_e
-    elif antena_c == 0:
-        antena_instalada = antena_a + antena_b + antena_d + antena_e
-    elif antena_d == 0:
-        antena_instalada = antena_a + antena_b + antena_c + antena_e
-    elif antena_e == 0:
-        antena_instalada = antena_a + antena_b + antena_c + antena_d
-    else:
-        antena_instalada = antena_a + antena_b + antena_c + antena_d + antena_e
-    return antena_instalada
+area_instalada_actual=float(input("Ingrese el area a  instalar en mts:  "))
+if area_instalada_actual == 0:
+        print("error en los datos")
     
+    
+resultado=(area_instalada_actual/antena_instalada)
+resultado=(int(resultado))
+    
+
+print(f"el número a antenas a instalar es: {resultado}")
+
+
+
+
+
